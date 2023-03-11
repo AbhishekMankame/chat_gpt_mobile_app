9f04e17b0458151c30c1b47104fa57bc5ebde401
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-11 22:03:12 +0530

    chatmessage_update

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class ChatMessage extends StatelessWidget {
   const ChatMessage({super.key, required this.text, required this.sender});
@@ -16,6 +19,20 @@ class ChatMessage extends StatelessWidget {
           margin: const EdgeInsets.only(right: 16.0),
           child: CircleAvatar(child: Text(sender[0])),
         ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              sender,
+              style: Theme.of(context).textTheme.subtitle1),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            )
+          ],
+        )),
       ],
     );
   }
