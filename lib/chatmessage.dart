ee52c804d52aa3fd09b9573f6b6456160a61081d
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-11 21:51:16 +0530

    widget_package_update

---
+++ lib/chatmessage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
