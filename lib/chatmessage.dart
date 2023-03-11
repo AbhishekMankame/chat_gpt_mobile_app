6c40f91fe2af517557cca4450151e0a166b7cbca
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-11 21:56:07 +0530

    container_widget_update

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -3,10 +3,19 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({super.key});
+  const ChatMessage({super.key, required this.text, required this.sender});
+
+  final String text;
+  final String sender;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(right: 16.0),
+        )
+      ],
+    );
   }
 }
